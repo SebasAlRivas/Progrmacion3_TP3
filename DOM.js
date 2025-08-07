4a6a869775e28a7d2b315ae15cefbf92aaf91377
SebasAlRivas <113698098+SebasAlRivas@users.noreply.github.com>
2025-08-07 04:43:22 +0000

    fin

--- DOM.js
+++ DOM.js
@@ -2,3 +2,19 @@ function cambiarTitulo() {
     let titulo = document.getElementById("titulo");
     titulo.textContent = "Hola Mundo con Javascript";
 }
+
+function cambiarEstilo() {
+    let titulo = document.getElementById("titulo");
+    titulo.style.color = "red";
+    titulo.style.textAlign = "center";
+}
+
+
+function creaDiv() {
+    let nuevoDiv = document.createElement("div");
+    nuevoDiv.textContent = "Javascript permite crear páginas dinámicas";
+    nuevoDiv.style.color = "white";
+    nuevoDiv.style.backgroundColor = "red";
+    nuevoDiv.style.textAlign = "center";
+    document.body.appendChild(nuevoDiv);
+}
